a5014b28cb7f17999b89f57bac2b06635cc5a83f
NickHlzv <nikitos.halzev@gmail.com>
2025-01-24 00:50:10 +0300

    Initialize main form objects, and start placing them

--- UI.py
+++ UI.py
@@ -3,6 +3,7 @@ import tkinter.ttk as ttk
 import logging
 import tkinter.messagebox as messagebox
 
+#Подключение логгирования модуля UI.py
 logging.basicConfig(level=logging.INFO, filename="app.log",filemode="w",
                     format="%(asctime)s %(levelname)s %(message)s")
 
@@ -10,34 +11,56 @@ logging.basicConfig(level=logging.INFO, filename="app.log",filemode="w",
 class UIMain(tk.Tk):
     def __init__(self, master=None, form_name="Test", form_size="1024x768"):
         super().__init__(master)
-        self.key_label, self.key_combobox, self.method_label, self.method_combobox = None, None, None, None
-        self.create_keybox(["arfasad-asdsa", "sgfdgfd-asda"])
-        self.create_methodbox(["Организации", "Терминалы"])
+        #Инициализация параметров главного окна
         self.title(form_name)
         self.resizable(False, False)
         self.geometry(form_size)
+        self.font = ("Arial", 10)
+        self.left_label_width = 15
+        self.editbox_width = 60
+        self.__api_methods = ("Организации (/api/1/organizations)", "Терминалы (/api/1/terminal_groups)", "Типы оплат (/api/1/payment_types)",
+                              "Типы заказов (/api/1/deliveries/order_types)", "Доставки по дате (/deliveries/by_delivery_date_and_status)",
+                              "Меню/Выгрузка меню (/1/nomenclature)", "Внешнее меню (/2/menu)", "Выгрузка из внешнего меню (/2/menu/by_id)")
 
-    def create_keybox(self, menu:list, name="API key", width=50, font=("Arial", 10)):
-        self.key_label = ttk.Label(self, text=name, width=width, font=font)
-        self.key_label.pack()
-        self.key_combobox = ttk.Combobox(self, values=menu, width=width, font=font)
-        self.key_combobox.pack()
+        #Инициализация объектов главного окна
+        #Метки (названия и статус полей)
+        self.org_label = tk.Label(self, text="Название", width=self.left_label_width, font=self.font)
+        self.key_label = tk.Label(self, text="API ключ", width=self.left_label_width, font=self.font)
+        self.method_label = tk.Label(self, text="API Запрос", width=self.left_label_width, font=self.font)
+        self.json_request_label = tk.Label(self, text="JSON", width=self.left_label_width, font=self.font)
+        self.json_tips_label = tk.Label(self, text="Подсказки по JSON", font=self.font)
+        self.json_status_label = tk.Label(self, text="<JSON Status>", font=("Arial", 14), fg="green")
+        self.json_response_label = tk.Label(self, text="JSON Ответ", font=self.font)
 
-    def create_methodbox(self, menu:list, name="API Запрос", width=50, font=("Arial", 10)):
-        self.method_label = ttk.Label(self, text=name, width=width, font=font)
-        self.method_label.pack()
-        self.method_combobox = ttk.Combobox(self, values=menu, width=width, font=font, state="readonly")
-        self.method_combobox.pack()
+        #Выпадающие списки
+        self.org_box = ttk.Combobox(self, width=self.editbox_width, font=self.font)
+        self.key_box = ttk.Combobox(self, width=self.editbox_width, font=self.font)
+        self.method_box = ttk.Combobox(self, width=self.editbox_width, font=self.font, values=self.__api_methods)
 
-        self.btnHello = ttk.Button(self, text="Пpивeтcтвoвaть\nпoльзoвaтeля")
-        self.btnHello.bind("<ButtonRelease>", self.say_hello)
-        self.btnHello.pack()
-        self.btnShow = ttk.Button(self)
-        self.btnShow["text"] = "Выход"
-        self.btnShow["command"] = self.destroy
-        self.btnShow.pack(side="bottom")
+        #Кнопки
+        self.db_append_btn = ttk.Button(self, text="Добавить в справочник ключ")
+        self.send_request_btn = ttk.Button(self, text="Выполнить запрос")
 
-    def say_hello(self, evt):
-        tk.messagebox.showinfo("Test", "Привет, пользователь!")
+        #Текстовые поля
+        self.json_request_box = tk.Text(self, width=self.editbox_width+self.left_label_width, height = 50, font=self.font,
+                                            wrap="none")
+        self.json_tips_box = tk.Text(self, width=30, height = 30, font=self.font, wrap="none")
+        self.json_response_box = tk.Text(self, width=30, height = 30, font=self.font, wrap="none")
+
+        logging.info("Main window objects initialized")
+
+        self.widgets_pack()
+        logging.info("Initialized objects placed on main window successfully")
+
+
+        #Функция отвечающая за ращмещение ранее объявленных объектов окна
+    def widgets_pack(self):
+        pady = 1
+        self.org_label.pack(anchor="nw", pady=pady)
+        self.org_box.pack(anchor="n", pady=pady)
+        self.key_label.pack(anchor="nw", pady=pady)
+        self.key_box.pack(anchor="n", pady=pady)
+        self.method_label.pack(anchor="nw", pady=pady)
+        self.method_box.pack(anchor="n", pady=pady)
 
 
--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-import tkinter
-
 from UI import *
 
-
-#root = tk.Tk()
-#app_menu = tkinter.ttk.Combobox(root)
-#tkinter.ttk.
-#app_menu.pack()
-#root.mainloop()
-#tk.Frame
-
-#frm = ttk.Frame(root, padding=10, borderwidth=1, relief=SOLID)
-#frm1 = ttk.Frame(root, padding = 10, borderwidth=1, relief=SOLID)
-#frm.grid(column=0, row=0)
-#frm1.grid(column=1, row=0)
-#ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-#ttk.Label(frm1, text="Hello World!").grid(column=0, row=0)
-#ttk.Button(frm1, text="Quit", command=root.destroy).grid(column=1, row=0)
-#root.mainloop()
-
 root = UIMain()
-root.geometry("1024x768")
-
-#table = ttk.Treeview(app, show="tree")
-
 root.mainloop()
 
 
