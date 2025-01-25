84235b7766235e7e0188d6be585b4c292a591760
NickHlzv <nikitos.halzev@gmail.com>
2025-01-25 17:24:02 +0300

    Refactor LoggerUI

--- UI.py
+++ UI.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 import logging
-import tkinter.messagebox as messagebox
 
 #Подключение логгирования модуля UI.py
-logging.basicConfig(level=logging.INFO, filename="app.log",filemode="w",
-                    format="%(asctime)s %(levelname)s %(message)s")
+
+loggerUI = logging.getLogger(__name__)
+loggerUI.setLevel(logging.INFO)
+
+handlerUI = logging.FileHandler(f"{__name__}.log")
+handlerUI.setFormatter(logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s"))
+loggerUI.addHandler(handlerUI)
 
 
 class UIMain(tk.Tk):
@@ -24,13 +28,13 @@ class UIMain(tk.Tk):
 
         #Инициализация объектов главного окна
         #Метки (названия и статус полей)
-        self.org_label = tk.Label(self, text="Название", width=self.left_label_width, font=self.font)
-        self.key_label = tk.Label(self, text="API ключ", width=self.left_label_width, font=self.font)
-        self.method_label = tk.Label(self, text="API Запрос", width=self.left_label_width, font=self.font)
-        self.json_request_label = tk.Label(self, text="JSON", width=self.left_label_width, font=self.font)
-        self.json_tips_label = tk.Label(self, text="Подсказки по JSON", font=self.font)
-        self.json_status_label = tk.Label(self, text="<JSON Status>", font=("Arial", 14), fg="green")
-        self.json_response_label = tk.Label(self, text="JSON Ответ", font=self.font)
+        self.org_label = ttk.Label(self, text="Название", width=self.left_label_width, font=self.font, anchor="w")
+        self.key_label = tk.Label(self, text="API ключ", width=self.left_label_width, font=self.font, anchor="w")
+        self.method_label = tk.Label(self, text="API Запрос", width=self.left_label_width, font=self.font, anchor="w")
+        self.json_request_label = tk.Label(self, text="JSON", width=self.left_label_width, font=self.font, anchor="w")
+        self.json_tips_label = tk.Label(self, text="Подсказки по JSON", font=self.font, anchor="w")
+        self.json_status_label = tk.Label(self, text="<JSON Status>", font=("Arial", 14), fg="green", anchor="w")
+        self.json_response_label = tk.Label(self, text="JSON Ответ", font=self.font, anchor="w")
 
         #Выпадающие списки
         self.org_box = ttk.Combobox(self, width=self.editbox_width, font=self.font)
@@ -47,20 +51,19 @@ class UIMain(tk.Tk):
         self.json_tips_box = tk.Text(self, width=30, height = 30, font=self.font, wrap="none")
         self.json_response_box = tk.Text(self, width=30, height = 30, font=self.font, wrap="none")
 
-        logging.info("Main window objects initialized")
+        loggerUI.info("Main window objects initialized")
 
         self.widgets_pack()
-        logging.info("Initialized objects placed on main window successfully")
+        loggerUI.info("Initialized objects placed on main window successfully")
 
 
         #Функция отвечающая за ращмещение ранее объявленных объектов окна
     def widgets_pack(self):
-        pady = 1
-        self.org_label.pack(anchor="nw", pady=pady)
-        self.org_box.pack(anchor="n", pady=pady)
-        self.key_label.pack(anchor="nw", pady=pady)
-        self.key_box.pack(anchor="n", pady=pady)
-        self.method_label.pack(anchor="nw", pady=pady)
-        self.method_box.pack(anchor="n", pady=pady)
+        self.org_label.place(x=2, y=3, )
+        self.org_box.place(x=2+self.editbox_width, y=3)
+        self.key_label.place(x=2, y=26)
+        self.key_box.place(x=2+self.editbox_width, y=26)
+        self.method_label.place(x=2, y=49)
+        self.method_box.place(x=2+self.editbox_width, y=49)
 
 
