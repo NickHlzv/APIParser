84a70af636680ea04acc882881b27f618a66793b
NickHlzv <nikitos.halzev@gmail.com>
2025-01-26 20:00:59 +0300

    UI constructed and created without processing logic

--- UI.py
+++ UI.py
@@ -7,10 +7,12 @@ import logging
 
 loggerUI = logging.getLogger(__name__)
 loggerUI.setLevel(logging.INFO)
-
-handlerUI = logging.FileHandler(f"{__name__}.log")
+handlerUI = logging.FileHandler(f"{__name__}.log", encoding='utf-8')
 handlerUI.setFormatter(logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s"))
 loggerUI.addHandler(handlerUI)
+
+#  Set general UI font
+ui_font = ("Arial", 10)
 
 #  Redefine new class of Textbox with simple RightClick menu from standard Textbox
 class TextContext(tk.Text):
@@ -39,16 +41,62 @@ class TextContext(tk.Text):
     def popup_paste(self):
         self.event_generate("<<Paste>>")
 
+
+#   Define class for dialog window that adds data to DB
+class DialogWindow(tk.Toplevel):
+    def __init__(self, form_name="Добавить API ключ", form_size="400x90", font=ui_font):
+        # Init objects for dialog window
+        super().__init__()
+        self.font = font
+        self.title(form_name)
+        self.resizable(False, False)
+        self.geometry(form_size)
+
+        self.orgLabel = tk.Label(self, text="Название", font=self.font, anchor="w")
+        self.keyLabel = tk.Label(self, text="API ключ", font=self.font, anchor="w")
+        self.orgBox = ttk.Combobox(self, font=self.font)
+        self.keyBox = ttk.Combobox(self, font=self.font)
+        self.dbAppendBtn = tk.Button(self, text="Добавить", font=self.font)
+
+        loggerUI.info("Dialog window objects initialized")
+
+        self.widgets_place()
+
+    # Function that places objects initialized earlier
+    def widgets_place(self):
+        self.orgLabel.place(x=2, y=3, width=80)
+        self.orgBox.place(x=80, y=3, width=300)
+        self.keyLabel.place(x=2, y=26, width=80)
+        self.keyBox.place(x=80, y=26, width=300)
+        self.dbAppendBtn.place(x=2, y=48, width=140)
+        loggerUI.info("Initialized objects placed on dialog window successfully")
+
+    def dismiss(self):
+        self.grab_release()
+        self.destroy()
+        loggerUI.info("Dialog window closed")
+
 #   Define main window class
+
 class UIMain(tk.Tk):
-    def __init__(self, master=None, form_name="Test", form_size="1024x768"):
-        super().__init__(master)
+    @staticmethod
+    def create_window():
+        loggerUI.info("Click on button 'Добавить в справочник ключ' on main window")
+        #  Create an object of dialog window from class
+        dialog_window = DialogWindow()
+        #  Intercept clicking on cross button in a right corner
+        dialog_window.protocol("WM_DELETE_WINDOW", lambda: dialog_window.dismiss())
+        #  Intercept of input on application forms to dialog window
+        dialog_window.grab_set()
+
+    def __init__(self, form_name="Test", form_size="1024x768", font=ui_font):
+        super().__init__()
 
         #  Initialize attributes of main window
         self.title(form_name)
         #self.resizable(False, False)
         self.geometry(form_size)
-        self.font = ("Arial", 10)
+        self.font = font
         self.option_add('*TCombobox*Listbox.font', self.font)
         self.__apiMethods = ("Организации (/api/1/organizations)", "Терминалы (/api/1/terminal_groups)", "Типы оплат (/api/1/payment_types)",
                               "Типы заказов (/api/1/deliveries/order_types)", "Доставки по дате (/deliveries/by_delivery_date_and_status)",
@@ -72,7 +120,7 @@ class UIMain(tk.Tk):
         self.methodBox = ttk.Combobox(self, font=self.font, values=self.__apiMethods)
 
         #Buttons
-        self.dbAppendBtn = tk.Button(self, text="Добавить в справочник ключ", font=self.font)
+        self.dbAppendBtn = tk.Button(self, text="Добавить в справочник ключ", font=self.font, command=UIMain.create_window)
         bold_font = tkfont.Font(family="Arial", size=12, weight="bold")
         self.sendRequestBtn = tk.Button(self, text="Выполнить", font=bold_font)
 
@@ -85,7 +133,6 @@ class UIMain(tk.Tk):
         loggerUI.info("Main window objects initialized")
 
         self.widgets_place()
-        loggerUI.info("Initialized objects placed on main window successfully")
 
 
         #Function that places objects initialized earlier
@@ -106,7 +153,10 @@ class UIMain(tk.Tk):
         self.jsonStatusLabel.place(relx=0.7, y=3, relwidth=0.3, height=55)
         self.jsonResponseLabel.place(relx=0.7, y=60, relwidth=0.3)
         self.jsonResponseBox.place(relx=0.62, y=80, relwidth=0.378, relheight=0.86)
+        loggerUI.info("Initialized objects placed on main window successfully")
 
+    def __del__(self):
+        loggerUI.info("Main window closed and application stopped")
 
 
 
