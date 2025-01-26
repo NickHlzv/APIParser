5df1434860b83aad416e317a5bfaef053a5f5713
NickHlzv <nikitos.halzev@gmail.com>
2025-01-26 16:26:35 +0300

    Refactoring variables and code class. And add some objects on window

--- UI.py
+++ UI.py
@@ -21,38 +21,37 @@ class UIMain(tk.Tk):
         #self.resizable(False, False)
         self.geometry(form_size)
         self.font = ("Arial", 10)
-        self.left_label_width = 15
-        self.editbox_width = 50
         self.option_add('*TCombobox*Listbox.font', self.font)
-        self.__api_methods = ("Организации (/api/1/organizations)", "Терминалы (/api/1/terminal_groups)", "Типы оплат (/api/1/payment_types)",
+        self.__apiMethods = ("Организации (/api/1/organizations)", "Терминалы (/api/1/terminal_groups)", "Типы оплат (/api/1/payment_types)",
                               "Типы заказов (/api/1/deliveries/order_types)", "Доставки по дате (/deliveries/by_delivery_date_and_status)",
                               "Меню/Выгрузка меню (/1/nomenclature)", "Внешнее меню (/2/menu)", "Выгрузка из внешнего меню (/2/menu/by_id)")
 
         #   Initialize graphic objects of main window
         #   Labels (heads of interactive objects)
-        self.org_label = tk.Label(self, text="Название", width=self.left_label_width, font=self.font, anchor="w")
-        self.key_label = tk.Label(self, text="API ключ", width=self.left_label_width, font=self.font, anchor="w")
-        self.method_label = tk.Label(self, text="API Запрос", width=self.left_label_width, font=self.font, anchor="w")
-        self.json_request_label = tk.Label(self, text="JSON", width=self.left_label_width, font=self.font, anchor="w")
-        self.json_tips_label = tk.Label(self, text="Подсказки по JSON", font=self.font, anchor="w")
-        self.json_status_label = tk.Label(self, text="Response Status", font=("Arial", 14), fg="green", anchor="e")
-        self.json_response_label = tk.Label(self, text="JSON Ответ", font=self.font)
-        self.url_label = tk.Label(self, text="URL: https://api-ru.iiko.services/api/1/deliveries/by_delivery_date_and_status", font=self.font, anchor="w")
+        self.orgLabel = tk.Label(self, text="Название", font=self.font, anchor="w")
+        self.keyLabel = tk.Label(self, text="API ключ", font=self.font, anchor="w")
+        self.methodLabel = tk.Label(self, text="API Запрос", font=self.font, anchor="w")
+        self.jsonRequestLabel = tk.Label(self, text="JSON", font=self.font, anchor="w")
+        self.jsonTipsLabel = tk.Label(self, text="Подсказки по JSON", font=self.font, anchor="w")
+        self.jsonStatusLabel = tk.Label(self, text="Response Status", font=("Arial", 14), fg="green", anchor="e")
+        self.jsonResponseLabel = tk.Label(self, text="JSON Ответ", font=self.font)
+        self.urlLabel = tk.Label(self, text="URL: https://api-ru.iiko.services/api/1/deliveries/by_delivery_date_and_status",
+                                  font=self.font, anchor="w")
 
         #Drop lists (Comboboxes)
-        self.org_box = ttk.Combobox(self, width=self.editbox_width, font=self.font)
-        self.key_box = ttk.Combobox(self, width=self.editbox_width, font=self.font)
-        self.method_box = ttk.Combobox(self, width=self.editbox_width, font=self.font, values=self.__api_methods)
+        self.orgBox = ttk.Combobox(self, font=self.font)
+        self.keyBox = ttk.Combobox(self, font=self.font)
+        self.methodBox = ttk.Combobox(self, font=self.font, values=self.__apiMethods)
 
         #Buttons
-        self.db_append_btn = tk.Button(self, text="Добавить в справочник ключ", font=self.font)
-        self.send_request_btn = tk.Button(self, text="Выполнить запрос")
+        self.dbAppendBtn = tk.Button(self, text="Добавить в справочник ключ", font=self.font)
+        self.sendRequestBtn = tk.Button(self, text="Выполнить")
 
         #Text boxes
-        self.json_request_box = tk.Text(self, width=self.editbox_width+self.left_label_width, height = 50, font=self.font,
+        self.jsonRequestBox = tk.Text(self, height = 50, font=self.font,
                                             wrap="none")
-        self.json_tips_box = tk.Text(self, width=30, height = 30, font=self.font, wrap="none")
-        self.json_response_box = tk.Text(self, width=30, height = 30, font=self.font, wrap="none")
+        self.jsonTipsBox = tk.Text(self, width=30, height = 30, font=self.font, wrap="none")
+        self.jsonResponseBox = tk.Text(self, width=30, height = 30, font=self.font, wrap="none")
 
         loggerUI.info("Main window objects initialized")
 
@@ -62,17 +61,19 @@ class UIMain(tk.Tk):
 
         #Function that places objects initialized earlier
     def widgets_place(self):
-        self.org_label.place(x=2, y=3, width=80)
-        self.org_box.place(x=80, y=3, relwidth=0.4)
-        self.key_label.place(x=2, y=26, width=80)
-        self.key_box.place(x=80, y=26, relwidth=0.4)
-        self.method_label.place(x=2, y=49, width=80)
-        self.method_box.place(x=80, y=49, relwidth=0.4)
-        self.json_status_label.place(relx=0.7, y=3,  relwidth=0.3, height=55)
-        self.db_append_btn.place(x=2, y=71, width=185)
-        self.url_label.place(x=2, y=97, width=450)
-        self.json_request_label.place(x=2, y=115, width=38)
-        #self.box
+        self.orgLabel.place(x=2, y=3, width=80)
+        self.orgBox.place(x=80, y=3, relwidth=0.4)
+        self.keyLabel.place(x=2, y=26, width=80)
+        self.keyBox.place(x=80, y=26, relwidth=0.4)
+        self.methodLabel.place(x=2, y=49, width=80)
+        self.methodBox.place(x=80, y=49, relwidth=0.4)
+        self.jsonStatusLabel.place(relx=0.7, y=3,  relwidth=0.3, height=55)
+        self.dbAppendBtn.place(x=2, y=71, width=185)
+        self.urlLabel.place(x=2, y=97, width=450)
+        self.jsonRequestLabel.place(x=2, y=115, width=38)
+        self.jsonRequestBox.place(x=2, y=135, relwidth=0.37, relheight=0.8)
+        self.jsonTipsBox.place(relx=0.37, y=135, relwidth=0.25, relheight=0.8)
+
 
 
 
